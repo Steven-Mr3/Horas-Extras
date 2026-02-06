a5d8248800b40cf7ea3b80065327c3b0da5789ae
Steven Martínez <124845088+Steven-Mr3@users.noreply.github.com>
2026-02-06 10:58:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Horas-Extras/', 
+    base: '/Horas-Extras/', // Obligatorio para GitHub Pages
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
